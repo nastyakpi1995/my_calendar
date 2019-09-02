e33810da85cfafcdd8f1a5c0b8af517f8fddef8b
Nastia Krat <nastyakpi1995@gmail.com>
2019-09-02 16:26:18 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,112 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FormDialog from './FormDialog';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import FullCalendar from '@fullcalendar/react';
+import interactionPlugin from '@fullcalendar/interaction';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css' // webpack must be configured to do this
+
+export default class DemoApp extends React.Component {
+  state = {
+    setOpen: false,
+    open: false, 
+    calendarEvents: [{
+      timeZone: 'local',
+      id: Date.now(),
+      title: 'new',
+      start: new Date(),
+      startEvent: false,
+      bacground: 'blue',
+    },
+    ],
+  }
+
+  handleChangeEvent = () => {
+    // this.setState({
+    //   calendarEvent: prevState.calendarEvents.map(
+    //     eventUnic => ({ 
+    //     ...eventUnic, 
+    //     title: eventUnic.id === +info.event.id ? 'Clicked' : eventUnic.title}))
+    // });  
+  }
+  
+
+  handleEvent = (info) => {
+    const eventObj = info.event;
+      if (eventObj.allDay) {
+        this.setState(prevState => ({
+          setOpen: true,
+          calendarEvents: prevState.calendarEvents.map(
+          eventUnic => ({ 
+          ...eventUnic, 
+          title: eventUnic.id === +info.event.id ? Date.now() : eventUnic.title}))
+          }))
+        info.jsEvent.preventDefault();
+      } else {
+          this.setState({
+            setOpen: false,
+          });
+      }
+  }
+
+  handleDateClick = (arg) => {
+    this.setState(prevState => ({
+      calendarEvents: [
+        ...prevState.calendarEvents,
+        {
+          timeZone: 'local',
+          title: prevState.title,
+          start: arg.date,
+          allDay: arg.allDay,
+          end: new Date(),
+          id: Date.now(),
+          bacground: arg.dayEl.style.backgroundColor = 'blue',
+        },
+      ],
+    }));
+  }
+
+  handleClickOpen = () => {
+    this.setState({
+      setOpen: true,
+    })
+  }
+
+  handleClose = () =>{
+    this.setState({
+      setOpen: false,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <FullCalendar 
+          defaultView="dayGridMonth" 
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]}
+          events={this.state.calendarEvents} 
+          dateClick={this.handleDateClick}
+          eventClick={this.handleEvent}
+          header={{
+            left: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+          }}
+        />
+        <FormDialog 
+          handleChangeEvent={this.handleChangeEvent}
+          handleClose={this.handleClose} 
+          calendarEvents={this.state.calendarEvents} 
+          setOpen={this.state.setOpen}
+        />
+
+        <header className="App-header">
+         
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/FormDialog.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Switch from '@material-ui/core/Switch';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+export default class FormDialog extends React.Component {
+  state = {
+    mapOnInfo: { 
+      fullWidth: true,
+      setFullWidth: true,
+      maxWidth: 'sm',
+      setMaxWidth: 'sm',
+      title: '',
+    }
+  }
+    
+  handleMaxWidthChange = (event) => {
+    const { value } = event.target;
+
+    this.setState({
+      mapOnInfo: {
+        setMaxWidth: value,
+      },
+    })
+  }
+
+  handleFullWidthChange = (event) => {
+    this.setState({
+      mapOnInfo: {
+        setFullWidth: event.target.checked,
+      },
+    })
+  }
+
+  render() {
+    const { handleClose, setOpen, handleChangeEvent, fullWidth, maxWidth } = this.props;
+
+    return (
+      <div>
+        <Dialog 
+          handleChangeEvent={handleChangeEvent}
+          open={setOpen} 
+          onClose={handleClose} 
+          fullWidth={fullWidth}
+          maxWidth={maxWidth}
+          aria-labelledby="max-width-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">New Event</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe 
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Entered your event"
+              type="email"
+              fullWidth
+            />
+             <form 
+              noValidate>
+              <FormControl>
+                <InputLabel htmlFor="max-width">maxWidth</InputLabel>
+                <Select
+                  value={maxWidth}
+                  onChange={this.handleMaxWidthChange}
+                  inputProps={{
+                    name: 'max-width',
+                    id: 'max-width',
+                  }}
+                >
+                  <MenuItem value={false}>false</MenuItem>
+                  <MenuItem value="xs">xs</MenuItem>
+                  <MenuItem value="sm">sm</MenuItem>
+                  <MenuItem value="md">md</MenuItem>
+                  <MenuItem value="lg">lg</MenuItem>
+                  <MenuItem value="xl">xl</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControlLabel
+                control={
+                  <Switch checked={this.fullWidth} onChange={this.handleFullWidthChange} value="title" />
+                }
+                label="Full width"
+              />
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Subscribe
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
