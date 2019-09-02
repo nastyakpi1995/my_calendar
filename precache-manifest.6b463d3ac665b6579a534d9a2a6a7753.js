ab179fedf59cf00513928c1b5a5a3136aaffdaed
Nastia Krat <nastyakpi1995@gmail.com>
2019-09-02 13:09:52 +0300

    Updates

---
+++ precache-manifest.6b463d3ac665b6579a534d9a2a6a7753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520bfb76dd13784b35a0d81505c1865b",
+    "url": "/my_calendar/index.html"
+  },
+  {
+    "revision": "b30d410b69a1ca3e2209",
+    "url": "/my_calendar/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a12fedf7e954b21b75d6",
+    "url": "/my_calendar/static/js/2.58cea2ff.chunk.js"
+  },
+  {
+    "revision": "b30d410b69a1ca3e2209",
+    "url": "/my_calendar/static/js/main.20bbfe43.chunk.js"
+  },
+  {
+    "revision": "c340629e6a2cd4f072ec",
+    "url": "/my_calendar/static/js/runtime~main.ca4dda66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_calendar/static/media/logo.5d5d9eef.svg"
+  }
+]);
